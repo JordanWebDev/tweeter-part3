8f08ef8b62d15ca29aace8cdfd5e3fbfd4774c62
JordanWebDev <JordanWebDev@users.noreply.github.com>
2019-04-08 21:14:46 -0600

    facebook login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
 
         <title>Tweeter - the best ranking social media application across all platforms</title>
     </head>
-    <body>
+    <body class="home-backdrop">
         <div class="container">
             <div class="row mainpg">
                 <div class="col-6 containbird">
@@ -56,9 +56,10 @@
                 </div>
             </div>
         </div>
+        <div class"footer-index">
+            @include('partials.footer')
 
-        @include('partials.footer')
-
+        </div>
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
